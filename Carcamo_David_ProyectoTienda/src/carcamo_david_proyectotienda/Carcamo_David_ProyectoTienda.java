9f27d3d4e02e380ef87a3aab36023aba89443f83
DJCC006 <david.carcamo2006@gmail.com>
2025-04-30 17:44:33 -0600

    modificacion y evaluaciones en caja
    Se desarrolló un procedimiento para evaluar las opciones que ingrese el usuario y arrojar mensajes específicos en caso de que introduzca números negativos, caracteres, números pasados del limite de opciones, u opciones antes de abrir caja.

--- Carcamo_David_ProyectoTienda/src/carcamo_david_proyectotienda/Carcamo_David_ProyectoTienda.java
+++ Carcamo_David_ProyectoTienda/src/carcamo_david_proyectotienda/Carcamo_David_ProyectoTienda.java
@@ -4,6 +4,8 @@
  */
 package carcamo_david_proyectotienda;
 
+import java.util.Scanner;
+        
 /**
  *
  * @author David
@@ -14,7 +16,68 @@ public class Carcamo_David_ProyectoTienda {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        //Creación de variable que lleva seguimiento del estatus de la caja
+        boolean statusCaja= false; 
+        
+        // Creación de Menu Inicial y lectura
+        System.out.print("****Menu de Facturación****");
+        System.out.println("\n 1. Abrir Caja"
+        +"\n 2. Venta"+"\n 3. Compra"+"\n 4. Reporte"+"\n 5. Cierre Caja"+"\n 6.Salir");
+        
+        
+        System.out.println("**Porfavor abrir caja antes de seleccionar alguna opcion del menu (seleccione opcion 1)***");
+        Scanner lea= new Scanner(System.in);
+        lea.useDelimiter("\n");
+        System.out.println("Porfavor ingrese una opción: ");
+        
+        
+       //Estructura para evaluacion de estatus de caja y respuestas
+       while (statusCaja!=true){        
+        if(lea.hasNextInt()){
+           int opcionInicial = lea.nextInt();
+            if(opcionInicial<1){
+           System.out.println("No se aceptan numeros negativos. Porfavor ignrese una opcion valida: ");
+         }else if(opcionInicial>6){
+             System.out.println("Numero fuera del limite establecido(1-6). Porfavor ingrese una opcion valida: ");
+             
+         }else if (opcionInicial != 1){
+           System.out.println("Porfavor abra caja primero. Ingrese nuevamente una opcion: ");
+           
+           }else if(opcionInicial ==1) {
+            System.out.println("Caja abierta");
+            statusCaja = true;
+          }
+         }else{
+            System.out.println("Valor invalido. Porfavor ingrese un numero como opcion: ");
+            lea.next();
+        }
+        }
+                
+        
+    
+       
+       //Estructura para realizar procedimientos
+       System.out.println("Porfavor seleccione una opcion del menu: ");
+       int opcion = lea.nextInt();
+       
+       if (opcion == 1){
+           System.out.println("Aqui se muestra la opcion de ingresar efectivo");
+       }else if (opcion==2){
+           System.out.println("Aqui se inicia el procedimiento de venta");
+       }else if (opcion ==3){
+           System.out.println("Aqui se inicia el procedimiento de compra");
+       }else if (opcion == 4){
+           System.out.println("Aqui se inicia el procedimiento de reporte");
+       }else if (opcion ==5){
+           System.out.println("Aqui se inicia el procedimiento de cierre de caja");
+       }else if (opcion ==6){
+           System.out.println("Aqui se inicia el procedimiento de salir ");
+       }
+       
+        
+        
+        
     }
     
 }
